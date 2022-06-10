ba96ffd835287abf3888488e19fdef7c89b18326
AlessP93 <alespiscino@libero.it>
2022-06-10 09:34:24 +0200

    primo commit

---
+++ src/components/commons/searchFilms.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <input type="text" placeholder="Search Film" @filmFound="$emit('pippo', discovered )">
+        <button>search</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'searchFilms',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
